bdb1d26d2c9e89fb77fd3802b1034321f709843b
Dan <dan.nitu@ngagetalent.com>
2023-05-02 16:20:44 +0300

    fetch single listing

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
 
 function App() {
   return (
@@ -31,6 +32,10 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/category/:vategoryName/:listingId'
+            element={<Listing />}
+          />
         </Routes>
 
         <Navbar />
