758cba2aa5a6dc9ac7d1b2e2acbb01fcad9c36dd
Dan <dan.nitu@ngagetalent.com>
2023-05-02 16:55:55 +0300

    contatc landlord page

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -31,11 +32,12 @@ function App() {
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
-          <Route path='/create-listing' element={<CreateListing />} />
           <Route
-            path='/category/:vategoryName/:listingId'
+            path='/category/:categoryName/:listingId'
             element={<Listing />}
           />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
 
         <Navbar />
